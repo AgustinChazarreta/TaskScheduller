433f3eb349810b4fe71819152c9044b01a8b9322
Agustín Chazarreta <agustinchazarreta00@gmail.com>
2026-01-29 21:24:45 -0300

    Actualización de Category

--- task-scheduler/src/main/java/com/AgsCh/task_scheduler/model/Category.java
+++ task-scheduler/src/main/java/com/AgsCh/task_scheduler/model/Category.java
@@ -2,9 +2,18 @@ package com.AgsCh.task_scheduler.model;
 
 public enum Category {
 
-    CATEGORY_1,
-    CATEGORY_2,
-    CATEGORY_3,
-    CATEGORY_4
+    CATEGORY_1(1),
+    CATEGORY_2(2),
+    CATEGORY_3(3),
+    CATEGORY_4(4);
 
+    private final int hierarchy;
+    
+        Category(int hierarchy) {
+            this.hierarchy = hierarchy;
+        }
+    
+        public int hierarchy() {
+            return hierarchy;
+        }
 }
